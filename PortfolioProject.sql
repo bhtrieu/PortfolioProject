f69ad1de529716eaba472c2f8e6332fdfeb9723c
Brian Trieu <brian.trieu7@gmail.com>
2021-08-12 22:03:55 -0700

    Add files via upload

--- PortfolioProject.sql
+++ PortfolioProject.sql
@@ -6,66 +6,91 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
---Primary stats that will we be examining
-select location, date, total_cases, new_cases, total_deaths, population
-from PortfolioProject..CovidDeaths
-order by location, date
+--
+--
+--Cases statistic 
+--
+--
 
---Total cases vs Population
---Percentage of the population that got covid
-select location, date, total_cases, population, (total_cases/population) * 100 as percentage_infected
+--Percentage of population that got covid for a country over time
+select location, date, total_cases, population, (total_cases/population) * 100 as percentage_of_population_infected
 from PortfolioProject..CovidDeaths
 where continent is not null
 order by location, date
 
---Total deaths vs Total cases
---Percentage of people that had covid and died
+--List of countries and the percentage of their population that has covid in descending order
+select location, MAX(total_cases) as max_cases, population, MAX(total_cases/population) * 100 as percentage_of_population_infected
+from PortfolioProject..CovidDeaths
+where continent is not null
+group by location, population
+order by percentage_of_population_infected desc
+
+--List of countries with most cases in descending order
+select location, MAX(total_cases) as max_cases
+from PortfolioProject..CovidDeaths
+where continent is not null
+group by location
+order by max_cases desc
+
+--Continents with the most cases in descending order
+select location, MAX(total_cases) as max_cases
+from PortfolioProject..CovidDeaths
+where continent is null
+group by location
+order by max_cases desc
+
+--
+--
+--Death statistics
+--
+--
+
+--Percentage of people that had covid and died for a country over time
 select location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as death_percentage
 from PortfolioProject..CovidDeaths
 where continent is not null
 order by location, date
 
---List of countries with most cases
-select location, MAX(total_cases) as max_cases
+
+--List of countries and percentage of covid deaths in descending order
+with country_deaths(location, max_deaths, max_cases)
+as
+(
+select location, MAX(cast(total_deaths as int)) as max_deaths, MAX(total_cases) as max_cases 
 from PortfolioProject..CovidDeaths
 where continent is not null
 group by location
-order by max_cases desc
+)
+select *, (max_deaths/max_cases) * 100 as covid_death_percentage
+from country_deaths
+order by covid_death_percentage desc
 
---List of countries with most deaths
+--List of countries with most deaths in descending order
 select location, MAX(cast(total_deaths as int)) as max_deaths
 from PortfolioProject..CovidDeaths
 where continent is not null
 group by location
 order by max_deaths desc
 
---Countries with highest infection rate
-select location, MAX(total_cases) as max_cases, population, MAX(total_cases/population) * 100 as max_infection_rate
-from PortfolioProject..CovidDeaths
-where continent is not null
-group by location, population
-order by max_infection_rate desc
-
---Continents with the most cases
-select location, MAX(total_cases) as max_cases
-from PortfolioProject..CovidDeaths
-where continent is null
-group by location
-order by max_cases desc
-
---Continents with the most deaths
+--Continents with the most deaths in descending order
 select location, MAX(cast(total_deaths as int)) as max_deaths
 from PortfolioProject..CovidDeaths
 where continent is null
 group by location
 order by max_deaths desc
 
---Countries with most vaccinations
-select location, MAX(cast(total_vaccinations as int)) as max_vaccinations
+--
+--
+--Vaccination statistics
+--
+--
+
+--Countries with most people vaccinated
+select location, MAX(cast(total_vaccinations as int)) as people_vaccinated
 from PortfolioProject..CovidVaccinations
 where continent is not null
 group by location
-order by max_vaccinations desc
+order by people_vaccinated desc
 
 --Countries with most fully people vaccinated
 select location, MAX(cast(people_fully_vaccinated as int)) as max_fully_vaccinated
@@ -75,11 +100,18 @@ group by location
 order by max_fully_vaccinated desc
 
 --Continents with most vaccinated
-select location, MAX(cast(total_vaccinations as int)) as max_vaccinations
+select location, MAX(cast(total_vaccinations as float)) as max_vaccinations
 from PortfolioProject..CovidVaccinations
 where continent is null
 group by location
 order by max_vaccinations desc
+
+
+--
+--
+--GLOBAL NUMBERS
+--
+--
 
 --Global cases and deaths
 select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as global_death_percentage
@@ -87,10 +119,12 @@ from PortfolioProject..CovidDeaths
 where continent is not null
 
 
+--
+--
 --VIEWS
 --
 --
---
+
 GO
 
 --Total cases vs Population view
